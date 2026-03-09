110d41be18b8da14a3599411f5240691d06c784d
ceduardoz <ceduardoz957@gmail.com>
2026-03-09 01:16:47 -0300

    refactor(auth): update login schema email validation to align with latest Zod API and remove deprecation warning

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -7,12 +7,12 @@ import {
 
 const registerSchema = z.object({
   name: z.string().min(3, "O nome deve ter pelo menos 3 caracteres."),
-  email: z.string().email("email inválido."),
+  email: z.email("email inválido."),
   password: z.string().min(8, "A senha precisa ter pelo menos 8 caracteres."),
 });
 
 const loginSchema = z.object({
-  email: z.string().email("email inválido"),
+  email: z.email("email inválido"),
   password: z.string().min(8, "A senha precisa ter pelo menos 8 caracteres."),
 });
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,5 @@
 import { Router } from "express";
 
-import authRoutes from "./authRouter.js";
-
 const router = Router();
 
 router.get("/", (req, res) => {
