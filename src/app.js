91733f501f79075dbefecb73e6c92ff8b1eaeca1
ceduardoz <ceduardoz957@gmail.com>
2026-03-11 17:17:14 -0300

    docs: add swagger documentation for financial control api

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ import transactionRouter from "./routes/transactionRouter.js";
 import recurringTransactionRouter from "./routes/recurringTransactionRouter.js";
 import investmentRouter from "./routes/investmentRouter.js";
 import dashboardRouter from "./routes/dashboardRouter.js";
+import { swaggerDocument, swaggerUi } from "./routes/swagger.js";
 
 class App {
   constructor() {
@@ -31,6 +32,7 @@ class App {
     this.app.use("/recurring-transaction", recurringTransactionRouter);
     this.app.use("/investments", investmentRouter);
     this.app.use("/dashboard", dashboardRouter);
+    this.app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
   }
 }
 
