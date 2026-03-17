6467c035c901d3ab6e191d63e2ab5ceba91b3845
ceduardoz <ceduardoz957@gmail.com>
2026-03-16 23:55:31 -0300

    fix: authSchema update for login and registration validation

--- src/schemas/authSchemas.js
+++ src/schemas/authSchemas.js
@@ -2,11 +2,11 @@ import { z } from "zod";
 
 export const registerSchema = z.object({
   name: z.string().min(3, "O nome deve ter pelo menos 3 caracteres."),
-  email: z.email("email inválido."),
+  email: z.string().email("email inválido."),
   password: z.string().min(8, "A senha precisa ter pelo menos 8 caracteres."),
 });
 
 export const loginSchema = z.object({
-  email: z.email("email inválido"),
+  email: z.string().email("email inválido"),
   password: z.string().min(8, "A senha precisa ter pelo menos 8 caracteres."),
 });
